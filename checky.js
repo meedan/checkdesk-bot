e25adb3ac113945c8c37ce56be94d76fd5e61f7f
infojunkie <karim.ratib@gmail.com>
2015-04-27 17:33:43 -0700

    Refactor for easier testability

--- checky.js
+++ checky.js
@@ -5,29 +5,50 @@ var config = require('./config.js');
 var _ = require('underscore');
 var request = require('superagent');
 
-var t = new twit(config.twitter);
-var stream = t.stream('user', { with: 'user' });
-stream.on('tweet', function(tweet) {
-  // Only process tweets that the bot did not send and where it is mentioned.
-  if (tweet.user.screen_name === config.twitter.screen_name) return;
-  if (!_.filter(tweet.entities.user_mentions, function(um) { return um.screen_name === config.twitter.screen_name }).length) return;
-  sendToCheckdesk(tweet);
-});
+function Checky(config) {
+  this.config = config;
+  this.t = new twit(this.config.twitter);
+  this.stream = this.t.stream('user', { with: 'user' });
+  // TODO also detect direct messages
+  this.stream.on('tweet', this.processTweet.bind(this));
+}
 
-function sendToCheckdesk(tweet) {
+Checky.prototype.processTweet = function(tweet) {
+  var self = this;
+  // Only process tweets that the bot did not send and where it is mentioned.
+  if (tweet.user.screen_name === this.config.twitter.screen_name) return;
+  if (!_.filter(tweet.entities.user_mentions, function(um) { return um.screen_name === self.config.twitter.screen_name }).length) return;
+  this.sendToWebhook(tweet);
+}
+
+Checky.prototype.sendToWebhook = function(tweet) {
+  var self = this;
   // TODO log
+  // TODO security
   request
-    .post(config.checkdesk_url + '/api/v1/bot')
+    .post(this.config.webhook)
     .send({ tweet: tweet })
     .end(function(err, res) {
-        // Post back to Twitter.
       if (res.body.reply) {
-        var reply = res.body.reply;
-        reply.in_reply_to_status_id = tweet.id_str;
-        t.post('statuses/update', reply, function(err, data, res) {
-          console.log(err);
-          console.log(data);
-        });
+        self.tweetReply(res.body.reply, tweet);
+      }
+      else {
+        // TODO error handling
       }
     });
 }
+
+Checky.prototype.tweetReply = function(reply, tweet) {
+  var self = this;
+  var reply_tweet = {
+    status: reply,
+    in_reply_to_status_id: tweet.id_str
+  };
+  this.t.post('statuses/update', reply_tweet, function(err, data, res) {
+    // TODO log and error handling
+    console.log(err);
+    console.log(data);
+  });
+}
+
+var checky = new Checky(config);
